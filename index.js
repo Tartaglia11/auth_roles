decd62d3c0aeeee1b79e3429ed6e4b0971cf433a
Rinat Uzanzaev <wilax175@gmail.com>
2023-01-09 18:29:43 +0600

    connect with database

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-const start = () => {
+app.use(express.json());
+
+const start = async() => {
 	try {
+		await mongoose.connect('mongodb+srv://qwerty:qwerty123@cluster0.bqc5cbi.mongodb.net/auth_roles?retryWrites=true&w=majority')
 		app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 	} catch (e) {
 		console.log(e);
